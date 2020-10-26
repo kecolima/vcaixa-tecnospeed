38a43fde40267bd4f7b4c7013396ebc811c1933f
kecolima <65935357+kecolima@users.noreply.github.com>
2020-10-26 17:24:31 -0300

    Delete conexao.php

--- ap1/v1/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php      
-        $obj_mysqli = new mysqli("localhost", "root", "root", "vcaixa");
-
-        if ($obj_mysqli->connect_errno){
-            echo "Ocorreu um erro na conexão com o banco de dados.";
-            exit;
-        }
-?>        
