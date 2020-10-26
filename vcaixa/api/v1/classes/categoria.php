82f47357019c36764e741899fdeb1f6d3cd7f751
kecolima <65935357+kecolima@users.noreply.github.com>
2020-10-26 03:00:13 -0300

    Create categoria.php

---
+++ vcaixa/api/v1/classes/categoria.php
@@ -0,0 +1,23 @@
+<?php 
+ 
+ class Categoria{
+     
+     public function criar($parametros){
+         
+         require_once 'conexao.php';
+         
+         $nome = $parametros['nome'];         
+        
+         $stmt = $obj_mysqli->prepare("INSERT INTO `categoria`(`nome`) VALUES (?)");          
+         $stmt->bind_param('s', $nome); 
+
+         if(!$stmt->execute()){
+             $erro = $stmt->error;
+         }else{
+             $sucesso = "Categoria cadastrada com sucesso!";
+         }
+         
+         return @$parametros;
+     } 
+ }
+ 
